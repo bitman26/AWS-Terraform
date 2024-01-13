3aca70a2e454a5dc80ec35e0d301f433475f30d8
bitman26 <rafaelnogueira@msn.com>
2024-01-13 16:39:28 -0300

    atualizando

--- main.tf
+++ main.tf
@@ -29,8 +29,15 @@ resource "aws_instance" "ec2" {
   ami           = "ami-0c0746ac7168488ae"
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.terraform-subnet-01.id
+  key_name      = "jenkins"
 
   tags = {
     Name = "terraform-debian-01"
   }
 }
+
+resource "aws_eip" "lb" {
+  instance = aws_instance.ec2.id
+  domain   = aws_vpc.terraform-vpc.id
+}
+
