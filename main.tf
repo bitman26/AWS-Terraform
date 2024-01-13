c20858da00a923d9a16df07c8a44310ff301ed65
Rafael Torres <33812794+bitman26@users.noreply.github.com>
2024-01-13 15:46:24 -0300

    update01

--- main.tf
+++ main.tf
@@ -1,23 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.32.0"
-    }
-  }
-}
-
-provider "aws" {
-    region = "sa-east-1"
-
-  }
 
   resource "aws_s3_bucket" "example" {
   bucket = "bit-bucket3"
 
   tags = {
     Name        = "teste-terraform"
-    Environment = "Dev"
   }
 }
 
