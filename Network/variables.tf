0cc62e73dd68ace0c2e9a5bd95857f541880f2eb
Rafael Torres <33812794+bitman26@users.noreply.github.com>
2024-02-10 15:40:57 -0300

    corrigindo ambiente

---
+++ Network/variables.tf
@@ -0,0 +1,11 @@
+variable "vpc-range" { 
+   default =  "192.168.0.0/24" 
+    }
+
+variable "default-route" { 
+   default =  "0.0.0.0/0" 
+    }
+    
+variable "my-ip" { 
+   default =  "177.95.55.9/32"
+    }
