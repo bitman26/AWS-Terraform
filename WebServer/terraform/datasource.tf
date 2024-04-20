4bdc1eb84eb9c7ecca9421d71f5638805ee3d437
Rafael Torres <33812794+bitman26@users.noreply.github.com>
2024-04-19 21:16:12 -0300

    Update datasource.tf

--- WebServer/terraform/datasource.tf
+++ WebServer/terraform/datasource.tf
@@ -2,10 +2,10 @@ data "aws_s3_bucket" "tfstate-bucket" {
   bucket = "bit-tfstate-bucket"
 }
 
-data "aws_subnet" "terraform-subnet-01" {
+data "aws_subnet" "terraform-subnet" {
   name = terraform-subnet-01
 }
 
-data "aws_security_group" "terraform-sg" {
+data "aws_security_group" "sg-terraform" {
   name =  terraform-sg
 }
