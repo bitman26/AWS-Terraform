23c269c2a051ed0c4aeee323f9068b6c2d748dc6
Rafael Torres <33812794+bitman26@users.noreply.github.com>
2024-04-19 21:01:34 -0300

    Update ec2.tf

--- WebServer/terraform/ec2.tf
+++ WebServer/terraform/ec2.tf
@@ -11,7 +11,7 @@
 resource "aws_instance" "ec2" {
   ami           = "ami-0c0746ac7168488ae"
   instance_type = "t3.micro"
-  subnet_id     = aws_subnet.terraform-subnet-01.id
+  subnet_id     = data.terraform-subnet-01
   key_name      = "jenkins"
   tags = {
     Name = "terraform-debian-01"
